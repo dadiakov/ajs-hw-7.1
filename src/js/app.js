b03e8e88d4dfe0e3c3932256685452701390daba
Dmitry Diakov <d.diakov@list.ru>
2021-06-29 22:53:33 +0500

    one symbol test add

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 export default class Validator {
   static validateUsername(name) {
+    if (name.length === 1) {
+      return /[a-z]/i.test(name);
+    }
     return !/[^\w-]/.test(name) && /^[^\d-_]([\w-]+)?[^\d-_]$/.test(name) && !/\d{3,}/.test(name);
   }
 }
